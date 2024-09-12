77ac8994c832fb2c416e82754496900211ffbf41
deadprogram <ron@hybridgroup.com>
2024-09-12 14:27:04 +0200

    examples: cleanup the multi example Rust module for brevity
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- examples/multi/modules/processrs/src/lib.rs
+++ examples/multi/modules/processrs/src/lib.rs
@@ -4,23 +4,15 @@ extern crate core;
 extern crate wee_alloc;
 extern crate alloc;
 
-use alloc::{string::String};
+use alloc::string::String;
 use alloc::string::ToString;
-use alloc::format;
 
 #[path = "../../../../../components/rust/cv.rs"]
 mod cv;
 
 #[no_mangle]
 pub extern fn process(mat: cv::Mat) -> cv::Mat {
-    let cols: String = String::from("Cols:");
-    let rows: String = String::from("Rows:");
-
-    let col: String = mat.cols().to_string();
-    let row: String = mat.rows().to_string();
-
-    let msg = format!("{cols} {col} {rows} {row}");
-    println(&msg);
+    println(&["Cols: ", &mat.cols().to_string(), " Rows: ", &mat.rows().to_string()].concat());
 
     return mat;
 }
