ad17c485dd5d4ec2353a5857ee3b923938506732
deadprogram <ron@hybridgroup.com>
2024-09-27 09:41:33 +0200

    examples: update for latest wit definitions
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- examples/multi/modules/processc/build.sh
+++ examples/multi/modules/processc/build.sh
@@ -6,7 +6,7 @@ WASI_LIBC_SYSROOT=$(tinygo env TINYGOROOT)
 /opt/wasi-sdk/bin/clang --target=wasm32-unknown-unknown -O3 \
         --sysroot="${WASI_LIBC_SYSROOT}/lib/wasi-libc/sysroot" \
         -z stack-size=4096 -Wl,--initial-memory=65536 \
-        -o ../processc.wasm process.c itoa.c ../../../../components/c/cv.c ../../../../components/c/cv_component_type.o \
+        -o ../processc.wasm process.c itoa.c ../../../../components/c/wasmcv/imports.c ../../../../components/c/wasmcv/imports_component_type.o \
         -Wl,--export=process \
         -Wl,--export=__data_end -Wl,--export=__heap_base \
         -Wl,--strip-all,--no-entry \
